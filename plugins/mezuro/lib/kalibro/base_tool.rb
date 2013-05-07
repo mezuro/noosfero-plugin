667a19b8dc0c0e6b33dd3573387bffa7618d5dee
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2013-05-07 14:33:13 -0300

    [Mezuro] Added test for Kalibro::BaseTool.all

--- plugins/mezuro/lib/kalibro/base_tool.rb
+++ plugins/mezuro/lib/kalibro/base_tool.rb
@@ -2,19 +2,6 @@ class Kalibro::BaseTool < Kalibro::Model
 
   attr_accessor :name, :description, :collector_class_name, :supported_metric
 
-  def self.find_by_name(base_tool_name)
-    new request(:get_base_tool, {:base_tool_name => base_tool_name})[:base_tool]
-  end
-
-  def self.all
-    basetools = all_names
-    basetools.map{ |name| find_by_name(name) }
-  end
-
-  def self.all_names
-    request(:all_base_tool_names)[:base_tool_name].to_a
-  end
-
   def supported_metric=(value)
     @supported_metric = Kalibro::Metric.to_objects_array value
   end
@@ -29,6 +16,19 @@ class Kalibro::BaseTool < Kalibro::Model
 
   def metric(name)
     supported_metrics.find {|metric| metric.name == name}
+  end
+
+  def self.find_by_name(base_tool_name)
+    new request(:get_base_tool, {:base_tool_name => base_tool_name})[:base_tool]
+  end
+  
+  def self.all_names
+    request(:all_base_tool_names)[:base_tool_name].to_a
+  end
+
+  def self.all
+    basetools = all_names
+    basetools.map{ |name| find_by_name(name) }
   end
 
 end
--- plugins/mezuro/test/unit/kalibro/base_tool_test.rb
+++ plugins/mezuro/test/unit/kalibro/base_tool_test.rb
@@ -12,16 +12,23 @@ class BaseToolTest < ActiveSupport::TestCase
     assert_equal @hash[:name], Kalibro::BaseTool.new(@hash).name
   end
   
+  should 'get base tool by name' do
+    request_body = {:base_tool_name => @base_tool.name}
+    Kalibro::BaseTool.expects(:request).with(:get_base_tool, request_body).returns({:base_tool => @hash})
+    assert_equal @base_tool.name, Kalibro::BaseTool.find_by_name(@base_tool.name).name
+  end
+
+  should 'get all base tools' do
+    names = ['Analizo', 'Checkstyle']
+    Kalibro::BaseTool.expects(:all_names).returns(names)
+    Kalibro::BaseTool.expects(:find_by_name).twice.returns(@base_tool)
+    assert_equal [@base_tool, @base_tool], Kalibro::BaseTool.all
+  end
+  
   should 'get base tool names' do
     names = ['Analizo', 'Checkstyle']
     Kalibro::BaseTool.expects(:request).with(:all_base_tool_names).returns({:base_tool_name => names})
     assert_equal names, Kalibro::BaseTool.all_names
   end
 
-  should 'get base tool by name' do
-    request_body = {:base_tool_name => @base_tool.name}
-    Kalibro::BaseTool.expects(:request).with(:get_base_tool, request_body).returns({:base_tool => @hash})
-    assert_equal @base_tool.name, Kalibro::BaseTool.find_by_name(@base_tool.name).name
-  end
-  
 end
