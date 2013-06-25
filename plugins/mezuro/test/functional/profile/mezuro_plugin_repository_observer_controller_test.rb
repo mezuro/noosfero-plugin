eddabee887e9b732ab20f01368f6f874cb3a9ad5
Alessandro Palmeira + Wilson Kazuo Mizutani <alessandro.palmeira@gmail.com>
2013-06-25 14:32:15 -0300

    [Mezuro] Finished repository observer controller tests

--- plugins/mezuro/test/functional/profile/mezuro_plugin_repository_observer_controller_test.rb
+++ plugins/mezuro/test/functional/profile/mezuro_plugin_repository_observer_controller_test.rb
@@ -1,10 +1,6 @@
 require 'test_helper'
 
-require "#{RAILS_ROOT}/plugins/mezuro/test/fixtures/processing_fixtures"
-require "#{RAILS_ROOT}/plugins/mezuro/test/fixtures/throwable_fixtures"
-require "#{RAILS_ROOT}/plugins/mezuro/test/fixtures/repository_fixtures"
-require "#{RAILS_ROOT}/plugins/mezuro/test/fixtures/project_content_fixtures"
-require "#{RAILS_ROOT}/plugins/mezuro/test/fixtures/configuration_fixtures"
+require "#{RAILS_ROOT}/plugins/mezuro/test/fixtures/processing_observer_fixtures"
 
 class MezuroPluginRepositoryObserverControllerTest < ActionController::TestCase
 
@@ -14,16 +10,34 @@ class MezuroPluginRepositoryObserverControllerTest < ActionController::TestCase
     @response = ActionController::TestResponse.new
     @profile = fast_create(Community)
 
-    @content = MezuroPlugin::ProjectContent.new(:profile => @profile, :name => name)
-    @content.expects(:send_project_to_service).returns(nil)
-    @content.stubs(:solr_save)
-    @content.save
+    @project_content = MezuroPlugin::ProjectContent.new(:profile => @profile, :name => name)
+    @project_content.expects(:send_project_to_service).returns(nil)
+    @project_content.stubs(:solr_save)
+    @project_content.save
   end
 
   should 'set variables to create a new repository observer' do
+    repository_id = "2"
 
-    get :new, :profile => @profile.identifier, :id => @content.id
+    get :new, :profile => @profile.identifier, :id => @project_content.id,  :repository_id => repository_id
 
+    assert_equal @project_content.id, assigns(:project_content).id
+    assert_equal repository_id, assigns(:repository_id)
+
+    assert_response :success
   end
+
+  should 'save repository observer and redirect back to repository page' do
+    fixture = ProcessingObserverFixtures
+    repository_observer = fixture.processing_observer
+
+    Kalibro::ProcessingObserver.expects(:new).with(fixture.processing_observer_hash).returns(repository_observer)
+    repository_observer.expects(:save).returns(true)
+
+    get :save, :profile => @profile.identifier, :id => @project_content.id,  :repository_id => repository_observer.repository_id, :repository_observer => repository_observer.to_hash
+
+    assert_response :redirect
+  end
+
 
 end
